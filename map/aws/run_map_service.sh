00c27cbdbc6bdb2666d8a1389cf9c09b5b65cda3
root <mingjiel@nvidia.com>
2026-07-10 21:12:12 -0500

    Added swap to map.

--- map/aws/run_map_service.sh
+++ map/aws/run_map_service.sh
@@ -2,6 +2,22 @@ log() {
     echo "$(date): $*"
 }
 
+# Create 16GB swap file to handle large data extractions.
+if [ ! -f /swapfile ]; then
+    log "Creating 16GB swap file"
+    fallocate -l 16G /swapfile
+    chmod 600 /swapfile
+    mkswap /swapfile
+    swapon /swapfile
+    if ! grep -q '^/swapfile ' /etc/fstab; then
+        echo '/swapfile none swap sw 0 0' >> /etc/fstab
+    fi
+fi
+
+# Check to fix permissions issues to nominatim data volume
+# chown -R 101:103 /var/lib/docker/volumes/nominatim-data/_data
+# chmod 700 /var/lib/docker/volumes/nominatim-data/_data
+
 log "Starting Nominatim geocoding server..."
 docker run --name nominatim --restart unless-stopped \
     --memory=4g --memory-swap=8g \
